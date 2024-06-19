86171c203d97a9d6a85990adbce6a943cf3416c5
Marcos Espinoza <marcosprogram96@gmail.com>
2024-06-19 10:00:27 -0600

    Practica #3 - Librerias

--- src/App.js
+++ src/App.js
@@ -8,30 +8,32 @@ import BackroundColor from './components/BackgroundColor';
 import SaludoPersonalizado from './components/SaludoPersonalizado';
 import Toats from './components/Toats';
 import Modal from './components/Modal';
+import Carousel from './components/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App" id='contenedor'>
-      <Titulo mensaje="Bienvenido al mundo React!" 
+      {/* <Titulo mensaje="Bienvenido al mundo React!" 
               style= {{ fontFamily: 'Arial ', fontSize: '20px', color: 'green'}}
-      />
-      <Parrafo texto="Esto es un parrafo"
+      /> */}
+      {/* <Parrafo texto="Esto es un parrafo"
                style= {{fontFamily: 'Georgia', fontSize: '5px', color: 'gray'}}
-      />
-      <Imagen 
+      /> */}
+      {/* <Imagen 
         url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAAN8QUDKGW2LTw8nhFC0uvsn-S-wIG1EJlw&s" 
         descripcion="Imagen de la empresa" 
         style= {{ width: '600px', height: 'auto', borderRadius:'20px'}}
-      />
+      /> */}
       
-      <Boton/>  
-      <ImagenBoton/>
-      <BackroundColor/>
+      {/* <Boton/>   */}
+      {/* <ImagenBoton/> */}
+      {/* <BackroundColor/> */}
       {/* <SaludoPersonalizado/> */}
       <Toats/>
       <Modal/>
+      <Carousel/>
 
     </div>
   );
