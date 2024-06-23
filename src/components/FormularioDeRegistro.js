14977d71f454ca0a81e6d5dec628c2cc385cdea4
Marcos Espinoza <marcosprogram96@gmail.com>
2024-06-23 15:44:58 -0600

    practica #5 terminada

--- src/components/FormularioDeRegistro.js
+++ src/components/FormularioDeRegistro.js
@@ -4,7 +4,6 @@ import { FormGroup, Label, Input, Modal, ModalHeader, ModalBody, ModalFooter, Bu
 import DatosModal from './DatosModal'; 
 
 const FormularioRegistro = () => {
-    //! Guardo el estado inicial de objebjetos en una variable
     const initialState = {
         nombre: '',
         apellido: '',
@@ -17,51 +16,39 @@ const FormularioRegistro = () => {
         notas: '',
         fechaRegistro: ''
     };
+    const [datos, setDatos] =useState(initialState) ;
 
-    //* Hago un estado donde incialmente el estado es la variable de objetos vacios
-    const [datos, setDatos] = useState(initialState);
-   
-
-
-    //* Estado para manejar el estado del Modal
+    
     const [modalOpen, setModalOpen] = useState(false);
     const actionModal = () => setModalOpen(!modalOpen);
 
-    //TODO: Funcion para cambiar el estado de los datos iniciales
-        const handleChange = (props) => {
-        const { input, value, type, checked } = props.target;
+    const handleChange = (props) => {
+        const { name, value, type, checked } = props.target;
         setDatos({
-            //! operador de propagación que se utiliza para copiar todas las propiedades existentes del objeto datos al nuevo objeto.
             ...datos,
-            //* si el input(type) es chekbox es tru, si no, se le asiga un valor que es el actual del input
-            [input]: type === 'checkbox' ? checked : value
+            [name]: type === 'checkbox' ? checked : value
         });
     };
 
-    //TODO: Funcion para abrir el modal
-    const handleSubmit = (props) => {
-        props.preventDefault(); //! // Evitar la recarga de la página
-        actionModal(); //! Abrir el modal al enviar el formulario
-        //* console.log(datos);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        actionModal(); // Abrir el modal al enviar el formulario
     };
 
+    
     //TODO: Funcion para Reiniciar los inputs
     const Reset = () => {
         setDatos(initialState);
-        // console.log(Reset);
+        // console.log(Reset);  
     };
-
 
     return (
         <div className="form-container">
             <h1>Formulario de Registro</h1>
-            {/* //!El atributo onChange asigna una función manejadora de eventos que se ejecuta cada vez que el valor del input cambia.  */}
-            <form onSubmit={handleSubmit} > 
+            <form onSubmit={handleSubmit}>
                 <FormGroup>
                     <Label className="fw-bold" for="name">Nombre</Label>
-                    {/* //! El atributo "value" vincula el valor del input al estado del componente. 
-                    //! Esto asegura que el input muestre el valor actual del estado y se actualice automáticamente cuando el estado cambie. */}
-                    <Input id="name" name="nombre" placeholder="Escribe tu nombre" value={datos.nombre} onChange={handleChange}   />
+                    <Input id="name" name="nombre" placeholder="Escribe tu nombre" value={datos.nombre}  onChange={handleChange} />
                 </FormGroup>
                 <FormGroup>
                     <Label className="fw-bold" for="lastName">Apellido</Label>
@@ -69,19 +56,19 @@ const FormularioRegistro = () => {
                 </FormGroup>
                 <FormGroup>
                     <Label className="fw-bold" for="Email">Email</Label>
-                    <Input id="Email" name="email" placeholder="example@gmail.com" value={datos.email} onChange={handleChange} />
+                    <Input id="Email" name="email" placeholder="example@gmail.com" value={datos.email}  onChange={handleChange} />
                 </FormGroup>
                 <FormGroup>
                     <Label className="fw-bold" for="password">Contraseña</Label>
-                    <Input id="password" type="password" name="password" placeholder="Contraseña" value={datos.password} onChange={handleChange} />
+                    <Input id="password" type="password" name="password" placeholder="Contraseña" value={datos.password}  onChange={handleChange} />
                 </FormGroup>
                 <FormGroup>
                     <Label className="fw-bold" for="age">Edad</Label>
-                    <Input id="age" type="number" name="edad" placeholder="8" value={datos.edad} onChange={handleChange} />
+                    <Input id="age" type="number" name="edad" placeholder="8" value={datos.edad}  onChange={handleChange} />
                 </FormGroup>
                 <FormGroup check>
                     <Label check>
-                        <Input name="genero" type="radio" value="masculino" checked={datos.genero === 'masculino'} onChange={handleChange} />
+                        <Input name="genero" type="radio" value="masculino" checked={datos.genero === 'masculino'}   onChange={handleChange} />
                         Hombre
                     </Label>
                 </FormGroup>
@@ -93,11 +80,12 @@ const FormularioRegistro = () => {
                 </FormGroup>
                 <FormGroup>
                     <Label className="fw-bold" for="Select">Select</Label>
-                    <Input id="Select" name="rol" type="select" value={datos.rol} onChange={handleChange}>
-                        <option onChange={handleChange} >Escuela</option>
-                        <option>Secundaria</option>
-                        <option>Preparatoria</option>
-                        <option>Universidad</option>
+                    <Input id="Select" name="rol" type="select"  value={datos.rol} onChange={handleChange}>
+                        <option value="">Selecciona una opción</option>
+                        <option value="Escuela">Escuela</option>
+                        <option value="Secundaria">Secundaria</option>
+                        <option value="Preparatoria">Preparatoria</option>
+                        <option value="Universidad">Universidad</option>
                     </Input>
                 </FormGroup>
                 <FormGroup check>
@@ -121,12 +109,11 @@ const FormularioRegistro = () => {
                     <Input id="date" type="date" name="fechaRegistro" value={datos.fechaRegistro} onChange={handleChange} />
                 </FormGroup>
                 <button type="submit" className="btn btn-primary">Mostrar</button>
-                <Button type="button" className="btn btn-secondary" onClick={Reset}>Resetear</Button>
+                <Button type="button" color="secondary" onClick={Reset} className="ms-2">Reiniciar</Button>
             </form>
             
-           
-            <DatosModal isOpen={modalOpen} toggle={actionModal}  datos={datos}/>
-            
+
+            <DatosModal isOpen={modalOpen} toggle={actionModal} datos={datos} />
         </div>
     );
 };
